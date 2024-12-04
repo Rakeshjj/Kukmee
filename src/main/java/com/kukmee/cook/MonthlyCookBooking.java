631367815ebe9bdb002d18b04f6e1c30a43b2dfb
stand <stand@LAPTOP-CI3OKN07>
2024-12-04 17:47:08 +0530

    Validations

--- src/main/java/com/kukmee/cook/MonthlyCookBooking.java
+++ src/main/java/com/kukmee/cook/MonthlyCookBooking.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +24,23 @@ public class MonthlyCookBooking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Date cannot be null")
+	@Pattern(regexp = "^([0-2][0-9]|3[01])-(0[1-9]|1[0-2])-\\d{4}$", message = "Date must be in the format DD-MM-YYYY")
     private String serviceStartDate;
+    
+    @Min(value = 1, message = "Number of people must be at least 1.")
+	@Max(value = 10, message = "Number of people cannot exceed 10.")
     private int numberOfPeople;
+    
+	@NotNull(message = "MealType cannot be null")
+	@Pattern(regexp = "^[^\\d].*", message = "MealType cannot start with a number")
     private String mealType; // Breakfast, Lunch, or Dinner
+	
+	@NotNull(message = "Location cannot be null")
+	@Pattern(regexp = "^[^\\d].*", message = "Address cannot start with a number")
     private String address; // Full address
+	
+	
     private double totalAmount;
     private double advancePayment;
     private double balanceAmount;
